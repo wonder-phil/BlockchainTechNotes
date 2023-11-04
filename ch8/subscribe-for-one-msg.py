4f1240d894eeea976d11c02184404be8157b8a0a
Phillip G. Bradford <phillip.g.bradford@gmail.com>
2023-11-04 13:00:29 -0400

    Update subscribe-for-one-msg.py
    cr/lf fix

--- ch8/subscribe-for-one-msg.py
+++ ch8/subscribe-for-one-msg.py
@@ -1,23 +1,23 @@
-import paho.mqtt.client as mqtt
-
-client = mqtt.Client()
-client.connect("localhost", 1883)# broker
-
-mydata = ""
-
-def on_message(client, userdata, message):
-  global mydata
-  print(message.topic + " " + message.payload.decode('utf-8'))
-  mydata = message.payload.decode('utf-8')
-
-client.on_message = on_message
-client.subscribe("test",qos=1)
-
-client.loop_start()
-
-while True:
-    if len(mydata) != 0:
-       break
-       
 
-client.loop_stop()
+import paho.mqtt.client as mqtt
+
+client = mqtt.Client()
+client.connect("localhost", 1883)  # broker on localhost!
+
+# global
+mydata = ""
+
+def on_message(client, userdata, message):
+  global mydata
+  print(message.topic + " " + message.payload.decode('utf-8'))
+  mydata = message.payload.decode('utf-8')
+
+client.on_message = on_message
+client.subscribe("test",qos=1)
+client.loop_start()
+while True:
+    if len(mydata) != 0:
+       break
+      
+client.loop_stop()
+
