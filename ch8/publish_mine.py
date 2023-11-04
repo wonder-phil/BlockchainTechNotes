53e5c5e4ff1ffc2d5e2202d81e5a620fafd7c86e
Phillip G. Bradford <phillip.g.bradford@gmail.com>
2023-11-04 12:49:27 -0400

    Update publish_mine.py
    newlines fixed

--- ch8/publish_mine.py
+++ ch8/publish_mine.py
@@ -1,1 +1,18 @@
-### From: "The Chains that bind us" by Phillip G Bradford#  https://github.com/wonder-phil/BlockchainTechNotes#      by P. G. Bradford#import paho.mqtt.client as mqttclient = mqtt.Client()client.connect("localhost", 1883)client.publish("mine", "Hello miner!")client.disconnect()
+#
+#
+# From: "The Chains that bind us" by Phillip G Bradford
+#  https://github.com/wonder-phil/BlockchainTechNotes
+#      by P. G. Bradford
+#
+import paho.mqtt.client as mqtt
+
+client = mqtt.Client()
+client.connect("localhost", 1883)
+client.publish("mine", "Hello miner!")
+
+client.disconnect()
+
+
+
+
+
