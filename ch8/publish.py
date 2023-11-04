fce062def69e4194c339d53aad7c1f4e55c936c5
Phil Bradford <phillip.g.bradford@gmail.com>
2023-11-04 12:41:00 -0400

    BlockMqtt and publishers

--- ch8/publish.py
+++ ch8/publish.py
@@ -1,7 +1,1 @@
-import paho.mqtt.client as mqtt
-
-client = mqtt.Client()
-client.connect("localhost", 1883)
-client.publish("test", "Hello, world!")
-
-client.disconnect()
+import paho.mqtt.client as mqttclient = mqtt.Client()client.connect("localhost", 1883)client.publish("mineData", "Hello, world!")client.disconnect()
