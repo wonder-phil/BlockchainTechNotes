de14fee4f557609719aa41fb3d5650a3aa44c4ed
Phil Bradford <phillip.g.bradford@gmail.com>
2023-11-04 12:46:27 -0400

    ch8

--- ch8/publish_blockData.py
+++ ch8/publish_blockData.py
@@ -1,1 +1,1 @@
-import paho.mqtt.client as mqttclient = mqtt.Client()client.connect("localhost", 1883)client.publish("blockData", "Hello, world!")client.disconnect()
+### From: "The Chains that bind us" by Phillip G Bradford#  https://github.com/wonder-phil/BlockchainTechNotes#      by P. G. Bradford#import paho.mqtt.client as mqttclient = mqtt.Client()client.connect("localhost", 1883)client.publish("blockData", "Hello, world!")client.disconnect()
